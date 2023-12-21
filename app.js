4bda1cf4c743183b3773768cc29b9ae7d7a58508
Arjun Raj <71980597+zuko046@users.noreply.github.com>
2023-12-21 09:16:15 +0530

    Your descriptive message here

--- app.js
+++ app.js
@@ -1,44 +1,31 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const cors = require('cors');''
+const dotenv = require("dotenv");
 const app = express();
-// const MONGODB_URI='mongodb+srv://arjun:HVUlAqvmeEAbOSTk@cluster0.deb5y4o.mongodb.net/LLT'
+const connectDB = require('./database/mongoConnection');
 
+const authRoutes = require('./routes/authRoute');
+const agentRoutes = require('./routes/agentRoute');
+const adminRoutes = require('./routes/adminRoute');
 
-require('dotenv').config();
+connectDB()
+dotenv.config();
+
 const corsOptions = {
-  origin: 'http://localhost:3000', // Replace with the actual origin of your React app
+  origin: 'http://localhost:3000', 
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204,
 };
 
-
-
-
-
-// Middleware
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
-// MongoDB connection
-mongoose.connect('mongodb+srv://arjun:HVUlAqvmeEAbOSTk@cluster0.deb5y4o.mongodb.net/LLT', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-// Routes
-const authRoutes = require('./routes/auth');
 app.use('/api/auth', authRoutes);
-// Routes
 
+app.use('/api/admin',adminRoutes)
 
-const userRoutes = require('./routes/user');
-app.use('/api/user', userRoutes);
-app.use('/api/userlist',userRoutes)
-// ...
+app.use('/api/agent', agentRoutes);
 
-// Start the server
-const PORT = 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(process.env.PORT,() => console.log(`Server is running on port ${process.env.PORT}`));
