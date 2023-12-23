7f558bfe26ad52259da2fce740b397c119052b04
Arjun Raj <71980597+zuko046@users.noreply.github.com>
2023-12-23 20:42:43 +0530

    nodemon change

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const authrouter = express.Router()
-const { login } = require("../controllers/authController")
+const express = require("express");
+const authrouter = express.Router();
+const { login } = require("../controllers/authController");
 
-authrouter.post('/register', login);
+authrouter.post("/login", login);
 
 module.exports = authrouter;
