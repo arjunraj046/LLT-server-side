61f5009118c4ae4a7993488a116a9572d8ac241c
Arjun Raj <71980597+zuko046@users.noreply.github.com>
2023-12-26 14:45:52 +0530

    12345

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const adminRoute = express.Router();
-const { agentRegister, agentList, agentDetails, editAgent, agentStatusChange, rangeSetup, rangeList } = require("../controllers/adminController");
+const { agentRegister, agentList, agentDetails, editAgent, agentStatusChange,listEntity, rangeSetup, rangeList } = require("../controllers/adminController");
 const { adminAuthMiddleware } = require("../middleware/authMiddleware");
 
 adminRoute.get("/agent-list/:filter?/:pagenumber?", agentList);
@@ -8,6 +8,9 @@ adminRoute.post("/agent-register", agentRegister);
 adminRoute.get("/agent/:id", agentDetails);
 adminRoute.put("/edit-agent/:id", editAgent);
 adminRoute.get("/agent-status-chnage/:id", agentStatusChange);
+
+adminRoute.get("/list-entity", listEntity);
+
 
 adminRoute.post("/enitity-rang", rangeSetup);
 adminRoute.get("/enitity-rang-list", rangeList);
